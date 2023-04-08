19bdb63cb8be6975fd0bc20681ab74a1392be4a4
Paulav Mukherjee <paulavmukherjee@gmail.com>
2023-04-09 01:16:16 +0530

    modified:   src/app/home/home.component.css modified:   src/app/home/home.component.html modified:   src/app/services/product.service.ts

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -11,7 +11,7 @@ export class ProductService {
 
   constructor(private http: HttpClient) { }
 
-  productAdding(data: any) {
+  productAdding(data: Product) {
     return this.http.post(this.url, data)
   }
   productListing() {
